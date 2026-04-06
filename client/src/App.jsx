d2c632b2da8845c792d25349fc5119c8a0683e9f
Niyatizzz <niyatikolukula@gmail.com>
2026-04-06 21:10:58 +0530

    deploy_102

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
+
+const AuthGitHubRedirect = () => {
+  useEffect(() => {
+    window.location.href = "https://forknight.onrender.com/auth/github";
+  }, []);
+
+  return null;
+};
 
 const App = () => {
   return (
@@ -10,6 +18,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/auth/github" element={<AuthGitHubRedirect />} />
         <Route path="/dashboard" element={<Dashboard />} />
         {/* Add other routes here, for example:
         <Route path="/about" element={<About />} />
